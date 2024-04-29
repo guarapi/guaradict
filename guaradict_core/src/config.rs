a651effae3338450d60efca59bbdaf9e498d1dd8
João Neto <joaopintoneto@gmail.com>
2024-04-28 23:20:29 -0300

    fix config tests/fixtures and change config struct types

--- guaradict_core/src/config.rs
+++ guaradict_core/src/config.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
-use serde_yaml::{self,  Number, Value};
+use serde_yaml::{self, Value};
 use regex::Regex;
 
 use crate::errors::ConfigFileError;
@@ -14,9 +14,8 @@ pub struct Config {
     pub name: String,
     pub ip: String,
     pub host: String,
-    pub port: Number,
-    // @TODO será usado futuramente
-    // pub database: Option<String>,
+    pub port: u16,
+    pub database: Option<String>,
     pub journal: Journal,
     pub replicas: Option<Vec<Replica>>,
 }
@@ -34,15 +33,14 @@ pub struct Replica {
     pub name: String,
     pub ip: String,
     pub host: String,
-    pub port: Number,
-    // @TODO será usado futuramente
+    pub port: u16,
     pub database: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct Journal {
     pub strategy: String,
-    pub size: Number,
+    pub size: u16,
 }
 
 pub fn parse_config_file(file_path: &str) -> Result<Config, ConfigFileError> {
@@ -50,9 +48,6 @@ pub fn parse_config_file(file_path: &str) -> Result<Config, ConfigFileError> {
     let reader = BufReader::new(file);
     let config: Config = serde_yaml::from_reader(reader).map_err(ConfigFileError::YamlError)?;
 
-    // println!("Configuração carregada: {:#?}", config.to_yaml_value());
-
-    // Ok(config)
     match validate_config(&config.to_yaml_value()) {
         Ok(()) => Ok(config),
         Err(e) => Err(ConfigFileError::ValidationError(e.to_string())),
@@ -84,9 +79,9 @@ pub fn validate_config(config: &Value) -> Result<(), Box<dyn Error>> {
         }
     }
 
-    let node_type = config.get(&Value::String("nodeType".into()))
-        .ok_or("Valor de nodeType inválido. Valores permitidos: 'primary', 'replica'")?
-        .as_str()
+    let node_type = config
+        .get(&Value::String("nodeType".into()))
+        .and_then(|value| value.as_str())
         .ok_or("Valor de nodeType inválido. Valores permitidos: 'primary', 'replica'")?;
 
     // Verifica se o tipo de nó é "primary" ou "replica"
@@ -96,67 +91,77 @@ pub fn validate_config(config: &Value) -> Result<(), Box<dyn Error>> {
     }
 
     // Verifica se o valor do 'ip' é um IP válido
-    if let Some(ip_value) = config.get(&Value::String("ip".into())) {
-        let ip_str = ip_value.as_str().ok_or("Valor de IP inválido")?;
-        if !is_valid_ip(ip_str) {
-            return Err("Valor de IP inválido".into());
-        }
-    } else {
-        return Err("Valor de IP ausente".into());
+    let ip_str = config
+        .get(&Value::String("ip".into()))
+        .and_then(|value| value.as_str())
+        .ok_or("Valor de IP inválido")?;
+    if !is_valid_ip(ip_str) {
+        return Err("Valor de IP inválido".into());
     }
 
     // Verifica se o 'port' está dentro do intervalo válido
-    if let Some(port_value) = config.get(&Value::String("port".to_string())) {
-        let port = port_value.as_i64().ok_or("Valor de porta inválido")?;
-        if port < 1 || port > 65535 {
-            return Err("Valor de porta inválido. A porta deve estar entre 1 e 65535".into());
-        }
-    } else {
-        return Err("Valor de porta ausente".into());
+    let port = config
+        .get(&Value::String("port".to_string()))
+        .and_then(|value| value.as_i64())
+        .ok_or("Valor de porta inválido")?;
+    if port < 1 || port > 65535 {
+        return Err("Valor de porta inválido. A porta deve estar entre 1 e 65535".into());
     }
 
-    // Verifica se o nome da base de dados está no formato correto
+    // Verifica se o nome da base de dados está no formato correto, se presente
     if let Some(database_name) = config.get(&Value::String("database".into())) {
-        let database_name_option = database_name.as_str();
-        if database_name_option.is_some() && !validate_database_name(database_name_option.ok_or("1Formato de nome de base de dados inválido")?) {
-            return Err("Formato de nome de base de dados inválido".into());
+        if !database_name.is_null() {
+            let database_name_str = database_name
+                .as_str()
+                .ok_or("Formato de nome de base de dados inválido")?;
+            if !validate_database_name(database_name_str) {
+                return Err("Formato de nome de base de dados inválido".into());
+            }
         }
     }
 
+    // Verifica as réplicas e o journal, se presentes
     let replicas = config.get(&Value::String("replicas".into()));
 
-    // Verifica se o campo "replicas" está válido
     if let Some(replicas) = replicas {
         if !replicas.is_null() {
-            let replicas_array = replicas.as_sequence().ok_or("Formato de réplicas inválido")?;
+            let replicas_array = replicas
+                .as_sequence()
+                .ok_or("Formato de réplicas inválido")?;
             for replica in replicas_array {
-                    // Verifica se o campo "journal" está presente em qualquer réplica
-                    if replica.get(&Value::String("journal".into())).is_some() {
-                        return Err("O campo 'journal' não é permitido dentro do campo 'replicas'".into());
-                    }
-
-                    // Valida cada réplica
-                    validate_config(replica)?;
+                if replica.get(&Value::String("journal".into())).is_some() {
+                    return Err("O campo 'journal' não é permitido dentro do campo 'replicas'".into());
+                }
+                validate_config(replica)?;
             }
         }
 
-        // Verifica as configurações do journal apenas para os nós raízes
-        if let Some(journal) = config.get(&Value::String("journal".into())) {
-            let journal_mapping = journal.as_mapping().ok_or("Invalid journal configuration")?;
-            let strategy = journal_mapping.get(&Value::String("strategy".into())).ok_or("Missing journal strategy")?;
-            let size = journal_mapping.get(&Value::String("size".into())).ok_or("Missing journal size")?;
+        // Se não houver "replicas", verifica se "journal" está presente e valida suas configurações
+        let journal = config
+            .get(&Value::String("journal".into()))
+            .ok_or("Chave obrigatória ausente: journal")?;
+        let journal_mapping = journal
+            .as_mapping()
+            .ok_or("Configuração de journal inválida")?;
+        let strategy = journal_mapping
+            .get(&Value::String("strategy".into()))
+            .ok_or("Estratégia de journal ausente")?;
+        let size = journal_mapping
+            .get(&Value::String("size".into()))
+            .ok_or("Tamanho de journal ausente")?;
 
-            let strategy_str = strategy.as_str().ok_or("Invalid journal strategy")?;
-            if !["async", "sync", "snapshot_log"].contains(&strategy_str) {
-                return Err("Invalid journal strategy value. Allowed values: 'async', 'sync', 'snapshot_log'".into());
-            }
+        let strategy_str = strategy
+            .as_str()
+            .ok_or("Estratégia de journal inválida")?;
+        if !["async", "sync", "snapshot_log"].contains(&strategy_str) {
+            return Err("Valor de estratégia de journal inválido. Valores permitidos: 'async', 'sync', 'snapshot_log'".into());
+        }
 
-            let size_int = size.as_i64().ok_or("Invalid journal size")?;
-            if size_int < 0 {
-                return Err("Invalid journal size value. Must be a non-negative integer".into());
-            }
-        } else {
-            return Err("Missing journal configuration".into());
+        let size_int = size
+            .as_i64()
+            .ok_or("Tamanho de journal inválido")?;
+        if size_int < 0 {
+            return Err("Valor de tamanho de journal inválido. Deve ser um inteiro não negativo".into());
         }
     }
 
--- guaradict_core/tests/config_test.rs
+++ guaradict_core/tests/config_test.rs
@@ -3,104 +3,105 @@ use guaradict_core::config::parse_config_file;
 #[test]
 fn test_parse_config_file_multi_primary_multi_replica() {
     let config = parse_config_file("tests/fixtures/multi-primary-multi-replica.yaml").unwrap();
-    assert_eq!(config.primary.is_some(), true);
-    assert_eq!(config.journal.is_some(), true);
+    println!("{:#?}", config);
+    assert_eq!(config.node_type, "primary");
+    assert_eq!(config.name, "primary-node-1");
+    assert_eq!(config.ip, "127.0.0.1");
+    assert_eq!(config.host, "127.0.0.1");
+    assert_eq!(config.port, 13141);
+    assert!(config.database.is_none());
+    assert!(config.replicas.is_some());
 
-    let primary = config.primary.unwrap();
-    assert_eq!(primary.len(), 2);
+    let replicas = config.replicas.unwrap();
+    assert_eq!(replicas.len(), 3);
 
-    let node1 = &primary[0];
-    assert_eq!(node1.name, "primary-node-1");
-    assert_eq!(node1.ip, "::0");
-    assert_eq!(node1.host, "::0");
-    assert_eq!(node1.database, "my-database-1");
-    assert!(node1.replicas.is_some());
-
-    let replica1 = &node1.replicas.as_ref().unwrap()[0];
-    assert_eq!(replica1.name, "replica-node-1");
+    let replica1 = &replicas[0];
+    assert_eq!(replica1.node_type, "primary");
+    assert_eq!(replica1.name, "primary-node-2");
     assert_eq!(replica1.ip, "127.0.0.1");
     assert_eq!(replica1.host, "127.0.0.1");
-    assert_eq!(replica1.database, "my-database-1");
+    assert_eq!(replica1.port, 13142);
+    assert!(replica1.database.is_none());
 
-    let node2 = &primary[1];
-    assert_eq!(node2.name, "primary-node-2");
-    assert_eq!(node2.ip, "::0");
-    assert_eq!(node2.host, "::0");
-    assert_eq!(node2.database, "my-database-2");
-    assert!(node2.replicas.is_some());
-
-    let replica2 = &node2.replicas.as_ref().unwrap()[0];
-    assert_eq!(replica2.name, "replica-node-2");
+    let replica2 = &replicas[1];
+    assert_eq!(replica2.node_type, "replica");
+    assert_eq!(replica2.name, "replica-node-1");
     assert_eq!(replica2.ip, "127.0.0.1");
     assert_eq!(replica2.host, "127.0.0.1");
-    assert_eq!(replica2.database, "my-database-2");
+    assert_eq!(replica2.port, 13143);
+    assert!(replica2.database.is_none());
 
-    let journal = config.journal.unwrap();
-    assert_eq!(journal.strategy, "sync");
+    let replica3 = &replicas[2];
+    assert_eq!(replica3.node_type, "replica");
+    assert_eq!(replica3.name, "replica-node-1");
+    assert_eq!(replica3.ip, "127.0.0.1");
+    assert_eq!(replica3.host, "127.0.0.1");
+    assert_eq!(replica3.port, 13144);
+    assert!(replica2.database.is_none());
 }
-
 
 #[test]
 fn test_parse_config_file_primary_multi_replica() {
     let config = parse_config_file("tests/fixtures/primary-multi-replica.yaml").unwrap();
-    assert_eq!(config.primary.is_some(), true);
-    assert_eq!(config.journal.is_some(), true);
+    assert_eq!(config.node_type, "primary");
+    assert_eq!(config.name, "primary-node-1");
+    assert_eq!(config.ip, "127.0.0.1");
+    assert_eq!(config.host, "127.0.0.1");
+    assert_eq!(config.port, 13141);
+    assert!(config.database.is_none());
+    assert!(config.replicas.is_some());
 
-    let primary = config.primary.unwrap();
-    assert_eq!(primary.len(), 1);
-
-    let node = &primary[0];
-    assert_eq!(node.name, "primary-node");
-    assert_eq!(node.ip, "::0");
-    assert_eq!(node.host, "::0");
-    assert_eq!(node.database, "my-database");
-    assert!(node.replicas.is_some());
-
-    let replicas = node.replicas.as_ref().unwrap();
+    let replicas = config.replicas.unwrap();
     assert_eq!(replicas.len(), 2);
 
     let replica1 = &replicas[0];
+    assert_eq!(replica1.node_type, "replica");
     assert_eq!(replica1.name, "replica-node-1");
-    assert_eq!(replica1.ip, "127.0.0.1");
+    assert_eq!(replica1.ip, "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff");
     assert_eq!(replica1.host, "127.0.0.1");
-    assert_eq!(replica1.database, "my-database");
+    assert_eq!(replica1.port, 13142);
+    assert!(replica1.database.is_none());
 
     let replica2 = &replicas[1];
+    assert_eq!(replica2.node_type, "replica");
     assert_eq!(replica2.name, "replica-node-2");
-    assert_eq!(replica2.ip, "::1");
-    assert_eq!(replica2.host, "::1");
-    assert_eq!(replica2.database, "my-database");
-
-    let journal = config.journal.unwrap();
-    assert_eq!(journal.strategy, "snapshot_log");
+    assert_eq!(replica2.ip, "127.0.0.1");
+    assert_eq!(replica2.host, "127.0.0.1");
+    assert_eq!(replica2.port, 13143);
+    assert!(replica2.database.is_none());
 }
-
 
 #[test]
 fn test_parse_config_file_primary_replica() {
     let config = parse_config_file("tests/fixtures/primary-replica.yaml").unwrap();
-    assert_eq!(config.primary.is_some(), true);
-    assert_eq!(config.journal.is_some(), true);
+    assert_eq!(config.node_type, "primary");
+    assert_eq!(config.name, "primary-node");
+    assert_eq!(config.ip, "127.0.0.1");
+    assert_eq!(config.host, "127.0.0.1");
+    assert_eq!(config.port, 13141);
+    assert_eq!(config.database, Some(String::from("my-database")));
+    assert!(config.replicas.is_some());
 
-    let primary = config.primary.unwrap();
-    assert_eq!(primary.len(), 1);
-
-    let node = &primary[0];
-    assert_eq!(node.name, "primary-node");
-    assert_eq!(node.ip, "::0");
-    assert_eq!(node.host, "::0");
-    assert_eq!(node.database, "my-database");
-    assert!(node.replicas.is_some());
-
-    let replicas = node.replicas.as_ref().unwrap();
+    let replicas = config.replicas.unwrap();
     assert_eq!(replicas.len(), 1);
 
     let replica = &replicas[0];
+    assert_eq!(replica.node_type, "replica");
     assert_eq!(replica.name, "replica-node-1");
     assert_eq!(replica.ip, "127.0.0.1");
     assert_eq!(replica.host, "127.0.0.1");
-    assert_eq!(replica.database, "my-database");
+    assert_eq!(replica.port, 13142);
+    assert_eq!(replica.database, Some(String::from("my-database")));
+}
 
-    let journal = config.journal.unwrap();
-    assert_eq!(journal.strategy, "async");
+#[test]
+fn test_parse_config_file_replica() {
+    let config = parse_config_file("tests/fixtures/replica.yaml").unwrap();
+    assert_eq!(config.node_type, "replica");
+    assert_eq!(config.name, "replica-node-1");
+    assert_eq!(config.ip, "127.0.0.1");
+    assert_eq!(config.host, "127.0.0.1");
+    assert_eq!(config.port, 13142);
+    assert_eq!(config.database, Some(String::from("my-database")));
+    assert!(config.replicas.is_none());
 }
